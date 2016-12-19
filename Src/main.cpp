17b63fd5e625f60d325111661442ae14a56af6e5
SaintRoge <lemondesansfil@gmail.com>
2016-12-19 12:56:36 +0100

    callShape.h and callShape.cpp added

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,16 +1,14 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Shape.hpp>
 
 using namespace sf;
 
-int main()
-{
-    RenderWindow window(VideoMode(200, 200), "Ouiiiii");
+int main() {
+    RenderWindow window(VideoMode(200, 200), "Little Test");
 
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == Event::Closed)
                 window.close();
         }
