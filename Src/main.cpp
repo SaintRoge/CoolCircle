21e0150cd38ce3c8dfefc703ef3e73ce49e92033
SaintRoge <lemondesansfil@gmail.com>
2016-12-19 14:19:03 +0100

    Color game Updated

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,11 +1,24 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics/Shape.hpp>
-#include "callShape.h"
+#include "fonctions.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace sf;
 
 int main() {
-    RenderWindow window(VideoMode(200, 200), "Little Test");
+    RenderWindow window(VideoMode(200, 200), "SFML works!");
+
+    srand (time(NULL));
+
+    int red(rand() % 266);
+    int green(rand() % 266);
+    int blue(rand() % 266);
+
+    CircleShape shape(100.f);
+
+    shape.setFillColor(Color(red, green, blue));
 
     while (window.isOpen()) {
         Event event;
@@ -15,6 +28,7 @@ int main() {
         }
 
         window.clear();
+        window.draw(shape);
         window.display();
     }
 
